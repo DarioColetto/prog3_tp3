f4833d1cea3c7658d0c0a88242d8ef8b0c3a9f72
Dario Coletto <darocoletto@gmail.com>
2025-06-04 02:06:07 -0300

    routes updated

--- src/routes/pacientes.route.js
+++ src/routes/pacientes.route.js
@@ -1,9 +1,9 @@
 const {Router} = require('express');
+const rutaPacientes = Router();
 const pacientesController = require('../controllers/API/pacientes.controller.js');
 const  {verifyTokenMiddleware}  = require('../middlewares/verifyToken.middleware.js');
-const rutaPacientes = Router();
+
 rutaPacientes.get('/', verifyTokenMiddleware ,pacientesController.list);
-rutaPacientes.post('/login',verifyTokenMiddleware,pacientesController.login)
 rutaPacientes.post('/',verifyTokenMiddleware ,pacientesController.create);
 rutaPacientes.put('/:id',verifyTokenMiddleware ,pacientesController.update);
 rutaPacientes.delete('/:id',verifyTokenMiddleware ,pacientesController.delete);
--- src/routes/turnos.route.js
+++ src/routes/turnos.route.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
+const router = Router();
 const turnosController = require("../controllers/API/turnos.controller.js");
 const { verifyTokenMiddleware } = require("../middlewares/verifyToken.middleware.js");
 
-const router = Router();
 
 router.get("/:idPaciente", verifyTokenMiddleware,turnosController.getByPaciente);
 router.delete("/:idTurno", verifyTokenMiddleware,turnosController.delete);
